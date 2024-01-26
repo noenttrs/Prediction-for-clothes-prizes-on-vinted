f350bd65ef3217bb8f46cd9cca9e9e3124a7688e
nono13616 <nottarisnoe@gmail.com>
2024-01-26 21:22:18 +0100

    Suppression of unused files

--- AI.js
+++
@@ -1,9 +0,0 @@
-const tf = require('@tensorflow/tfjs')
-const sk = require('scikitjs')
-const xslx = require('node-xlsx')
-
-sk.setBackend(tf)
-
-const file = xslx.parse('Clothes_filtered.xlsx')
-
-
--- converter.py
+++
@@ -1,5 +0,0 @@
-from sklearn2onnx import convert
-from skl2onnx.common.data_types import FloatTensorType
-
-initial_type = [('float_input', FloatTensorType([None, num_features]))]
-onnx_model = convert(model, initial_types=initial_type)
